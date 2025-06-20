473dc4dbbd3d55149311a6da25a8739960562af1
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-20 13:26:16 -0700

    Dude

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-answer=f"{number:.2f}"
+answer=f"Float: {number:.2f}"
 print(answer)
--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
-
+#Is there a new variable?
 print(str[39:66] + str[6] + str[106:110] + str[6] + str[0:6])
