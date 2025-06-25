ed7d3927c843ea4a7b2bb96bc23d06325dd2f586
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-25 16:09:42 +0200

    Leaders

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-	if i == 101 or i == 113:
-		continue
-	else:
-		ans = chr(i)
-		print("{}".format(ans), end="")
+    if i == 101 or i == 113:
+        continue
+    else:
+        out = chr(i)
+        print("{}".format(out), end="")
