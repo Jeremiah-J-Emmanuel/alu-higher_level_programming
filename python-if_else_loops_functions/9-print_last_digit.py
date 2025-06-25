f6b54b9d2d7f3718b515eefa53f7d9042142acd8
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-25 22:39:07 +0200

    Test

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,4 @@
 def print_last_digit(num):
     value = abs(num)
     last = value % 10
-    if num > 0:
-        print ("{}".format(last), end="")
-    else:
-        print ("-{}".format(last), end="")
+    print("{}".format(last), end="")
