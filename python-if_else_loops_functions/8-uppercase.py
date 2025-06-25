acc9ca2b1d3c2881994ecc5430a1799273819848
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-25 21:12:09 +0200

    This is the Night commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-	new = ""
-	for i in str:
-		if ord(i) in range(97,123):
-			j =  chr(ord(i) - 32)
-			new = new + j
-		else:
-			new = new + i
-	print (new)
-
+    new = ""
+    for i in str:
+        if ord(i) in range(97,123):
+            j =  chr(ord(i) - 32)
+            new = new + j
+        else:
+            new = new + i
+    print ("{}".format(new))
