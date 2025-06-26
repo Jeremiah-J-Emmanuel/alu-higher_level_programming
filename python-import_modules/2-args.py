95f1984e1222f9283163f11ce836f7f0ea02178c
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 16:40:05 +0200

    1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,11 +7,14 @@ def main():
 
     if length == 1:
         print("0 arguments.")
-
+    elif length == 2:
+        print("1 argument.")
+        print("1:", argv[1])
     else:
-        print(f"{length} argument:")
+        real_len = length - 1
+        print(f"{real_len} arguments:")
         count = 1
-        while count <= length:
+        while count <= real_len:
             item = argv[count]
             print (f"{count}: {item}")
             count = count + 1
