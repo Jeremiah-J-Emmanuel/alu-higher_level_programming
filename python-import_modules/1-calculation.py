cde815dcdbe06f832386083ea633cc9dde934076
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 18:42:25 +0200

    One linee

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div(a, b)
+from calculator_1 import add, sub, mul, div
 
 def main():
     a = 10
