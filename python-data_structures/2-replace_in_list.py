7e8a59a5d68b8ab2384a74db196a27ac4e90e7d6
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 22:54:11 +0200

    Ebere

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    range = (len(my_list) - 1)
+    idx = int(idx)
+    if idx < 0:
+        return my_list
+    elif idx > range:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
+
+
+if __name__ == "__main__":
+    replace_in_list(my_list, idx, element)
