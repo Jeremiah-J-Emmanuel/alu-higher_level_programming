33300f4403139595bd1ccffdabc35b1c4bc6b4f6
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 22:44:37 +0200

    Fixed

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,17 +2,15 @@
 
 def element_at(my_list, idx):
     idx = int(idx)
-    range = (len(my_list) - 1)
+    range = ((len(my_list)) - 1)
 
     if idx < 0:
         return None
     elif idx > range:
         return None
     else:
-        ans = (my_list[idx])
-        ans = int(ans)
-        return "{}".format(ans)
+        return int("{}".format(my_list[idx]))
 
 
 if __name__ == "__main__":
-    element_at(my_list, idx)
+    print("Element at index {:d} is {:d}".format(idx, element_at(my_list, idx)))
