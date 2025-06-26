a722301361a474b34887dbdf993444c57defbad0
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 23:10:56 +0200

    Tanzy

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse=True)
+    if type(my_list) != list:
+        print ""
+    else:
+        my_list.sort(reverse=True)
 
-    for i in my_list:
-        print("{:d}".format(i))
+        for i in my_list:
+            print("{:d}".format(i))
 
 
 if __name__ == "__main__":
