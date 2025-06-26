d9879f5891d487da52f7b702dde7c0469dae61d6
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 14:21:44 +0200

    This is the commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,14 +5,14 @@ def main():
 
     length = len(argv)
 
-    if length == 0:
+    if length == 1:
         print("0 arguments.")
 
     else:
         print(f"{length} argument:")
         count = 1
         while count <= length:
-            item = argv[(count - 1)]
+            item = argv[count]
             print (f"{count}: {item}")
             count = count + 1
 
