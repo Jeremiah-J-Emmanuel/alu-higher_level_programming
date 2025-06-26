4c5c257e1378a0c96243dedf303de538c981f528
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 23:11:38 +0200

    Yahweh

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if type(my_list) != list:
-        print ""
+        print ("")
     else:
         my_list.sort(reverse=True)
 
