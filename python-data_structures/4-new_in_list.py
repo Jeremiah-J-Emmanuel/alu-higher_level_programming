8685d3c02fbb59b3eaa626fb44162dd6e9b8dc27
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 23:54:17 +0200

    This  is the Love of Christ

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list
+    range = len(my_list) - 1
+    idx = int(idx)
+
+    if idx < 0:
+        return my_list
+    if idx > range:
+        return my_list
+    else:
+        new_list[idx] = element
+        return new_list
+
+
+if __name__ == "__main__":
+    new_in_list(my_list, idx, element)
