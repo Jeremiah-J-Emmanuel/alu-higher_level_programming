8e56fb25c93a1418d35954422d3dd5e24063e19d
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 14:15:38 +0200

    This is the code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
 from sys import argv
+def main():
 
-length = len(argv)
-if length == 0:
-    print("0 arguments.")
-else:
-    print(f"{length} argument:")
-    count = 1
-    while count <= length:
-        item = argv[count]
-        print (f"{count}: {item}")
+    length = len(argv)
+
+    if length == 0:
+        print("0 arguments.")
+
+    else:
+        print(f"{length} argument:")
+        count = 1
+        while count <= length:
+            item = argv[(count - 1)]
+            print (f"{count}: {item}")
+            count = count + 1
+
+main()
