76ebbfaeaeef41e78aaf811a89a793d0e878e6a6
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 09:12:15 -0700

    Yeah

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,11 +3,11 @@ from sys import argv
 
 def main():
     count = 0
-    for i in argv:
+    for i in argv[1:]:
         i = int(i)
         count = count + i
     
     print(count)
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     main()
