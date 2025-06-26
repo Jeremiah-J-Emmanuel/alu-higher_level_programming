c3fc5db5096dfeeac1e3caa5b85bb3386c969cab
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-26 13:12:00 +0200

    This is the Love of Christ

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-from add_0.py import add(a, b)
+def main():
+    from add_0.py import add(a, b)
 
+    a = 1
+    b = 2
+    answer = add(a, b)
+    print("{} + {} = {answer}".format(a, b, answer))
 
-a = 1
-b = 2
-answer = add(a, b)
-print("{} + {} = {answer}".format(a, b, answer))
+if "__name__" == "__main__":
+    main()
