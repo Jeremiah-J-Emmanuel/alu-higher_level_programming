2e74f6c3b489ec2a3665edbb3be6a56021b53078
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-28 00:58:01 +0200

    Re

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in  a_dictionary:
-        a_dictionary[key] = a_dictionary[key] ** 2
-    return(a_dictionary)
+    new_dictionary = a_dictionary.copy()
+    for key in  new_dictionary:
+        new_dictionary[key] = new_dictionary[key] ** 2
+    return(new_dictionary)
 
 
 if __name__ == "__main__":
