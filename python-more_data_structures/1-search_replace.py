d5ae7b1f5951f23a795a531ff8ddd87a94c30687
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 23:18:23 +0200

    Celebrate life

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     try:
-        new_list = my_list 
+        new_list = my_list[:]
         idx = new_list.index(search)
         new_list[idx] = replace
         return my_list
