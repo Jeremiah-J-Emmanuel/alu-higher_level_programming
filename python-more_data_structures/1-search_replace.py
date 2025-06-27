ddd09296135c2876a7310278436d608d21df6f7e
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 23:31:48 +0200

    Timee

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    try:
-        new_list = my_list[:]
-        idx = new_list.index(search)
-        new_list[idx] = replace
-        return new_list
-    except IndexError:
+    if len(my_list) == 0:
         return []
+    else:
+        try:
+            new_list = my_list[:]
+            count = 1
+            for i in new_list:
+                if i == search:
+                    idx = new_list[count]
+                    new_list[count] = replace
+                    count = count + 1
+                else:
+                    count = count + 1
+            return new_list
+        except ValueError:
+            return my_list
 
 
 if __name__ == "__main__":
