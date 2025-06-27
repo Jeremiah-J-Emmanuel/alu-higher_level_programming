b41a14d043ceda2d31bf5c26461984027acea72c
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-28 00:33:56 +0200

    Yeah

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,10 +2,8 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict_list = sorted(a_dictionary.items())
     sorted_dict = dict(sorted_dict_list)
-    for key in sorted_dict:
-        val = a_dictionary[key]
-        print(f"{key} : {val}")
-
+    for key, value in sorted_dict.items():
+        print(f"{key}: {value}")
 
 if __name__ == "__main__":
     print("Yes")
