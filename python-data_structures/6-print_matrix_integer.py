19f1e8c78bf6f9e43deca15ebdfab2739ceecaa6
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 22:24:32 +0200

    Way to Big

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            range = len(i) - 1
-            if i.index(j) < range:
-                print("{:d}".format(j), end=" ")
-            else:
-                print("{:d}".format(j))
+        if len(i) == 0:
+            print(" ")
+        else:
+            for j in i:
+                range = len(i) - 1
+                if i.index(j) < range:
+                    print("{:d}".format(j), end=" ")
+                else:
+                    print("{:d}".format(j))
 
 
 if __name__ == "__main__":
-    print_matrix_integer(matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print_matrix_integer(matrix=[[]])
