8eda2b280c14879dfb55e2827f21c64b6bafc442
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-28 00:14:16 +0200

    Hii

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_dict_list = sorted(a_dictionary.items())
+    sorted_dict = dict(sorted_dict_list)
+    print(sorted_dict)
+
+
+if __name__ == "__main__":
+    print("Yes")
