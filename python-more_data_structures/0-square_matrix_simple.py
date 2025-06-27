95f32042589c2fcffeaf17c1563512ef06867e41
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 23:02:03 +0200

    GitHub

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_list = []
+
+    if len(matrix) == 0:
+        return matrix
+
     for i in matrix:
         if len(i) == 0:
-            return("")
+            return(i)
         else:
             strapping = []
             for j in i:
                 sqr = j ** 2
                 strapping.append(sqr)
             new_list.append(strapping)
-            
+  
     return(new_list)
-    
+
 
 if __name__ == "__main__":
     square_matrix_simple(matrix=[])
