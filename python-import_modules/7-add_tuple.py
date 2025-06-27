3b8ca1379f51c5bfcd7ad38bdbeaedd2306c5400
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 15:52:10 +0200

    The tuple solution

--- python-import_modules/7-add_tuple.py
+++ python-import_modules/7-add_tuple.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    fix = (0,)
+    print(type(tuple_a))
+    if len(tuple_a) < 2:
+        tuple_a += fix
+    elif len(tuple_b) < 2:
+        tuple_b += fix
+    else:
+        pass
+
+    first_a = tuple_a[0]
+    sec_a = tuple_a[1]
+
+    first_b = tuple_b[0]
+    sec_b = tuple_b[1]
+
+    new_1 = first_a + first_b
+    new_2 = sec_a + sec_b
+
+    added = (new_1, new_2)
+    return (added)
+
+
+if __name__ == "__main__":
+    add_tuple(tuple_a=(), tuple_b=())
