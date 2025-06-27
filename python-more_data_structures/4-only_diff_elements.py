15766a0baee616ef3567451a04f02a8d3aa73276
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 23:51:23 +0200

    Member

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,12 +2,12 @@
 def only_diff_elements(set_1, set_2):
     unique = []
     for i in set_1:
-        if not i in set_2:
+        if i not in set_2:
             unique.append(i)
         else:
             pass
     for i in set_2:
-        if not i in set_1:
+        if i not in set_1:
             unique.append(i)
         else:
             pass
