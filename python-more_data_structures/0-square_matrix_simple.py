67fe99fb6c83aa738f3b7a71825b8534c0c2ec8b
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 22:57:06 +0200

    Hifif

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for i in matrix:
+        if len(i) == 0:
+            print("")
+        else:
+            strapping = []
+            for j in i:
+                sqr = j ** 2
+                strapping.append(sqr)
+            new_list.append(strapping)
+        return(new_list)
+    
+
+if __name__ == "__main__":
+    square_matrix_simple(matrix=[])
