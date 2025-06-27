28379658e670b2889e7a23361b491719057fc961
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 16:07:47 +0200

    Birdy

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    fix = (0,)
-    if len(tuple_a) < 2:
+    fix = (0, 0)
+    fix2 = (0,) 
+    if len(tuple_a) == 0:
         tuple_a += fix
-    elif len(tuple_b) < 2:
+    if len(tuple_b) == 0:
         tuple_b += fix
+    if  len(tuple_a) == 1:
+        tuple_a += fix2
+    elif len(tuple_b) == 1:
+        tuple_b += fix2
     else:
         pass
 
