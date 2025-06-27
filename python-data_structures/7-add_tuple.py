ee91f09de135e9db9cf0aa44a688d3ae43a743e7
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 16:35:09 +0200

    Done

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,13 +7,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += fix
     if len(tuple_b) == 0:
         tuple_b += fix
-    if  len(tuple_a) == 1:
+    if len(tuple_a) == 1:
         tuple_a += fix2
-    elif len(tuple_b) == 1:
+    if len(tuple_b) == 1:
         tuple_b += fix2
     else:
         pass
-
     first_a = tuple_a[0]
     sec_a = tuple_a[1]
 
@@ -28,4 +27,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
 
 if __name__ == "__main__":
-    add_tuple(tuple_a=(), tuple_b=())
+    print(add_tuple(tuple_a=(1,), tuple_b=(2,)))
