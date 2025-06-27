5387cfb6d5727d1718b8d5cf9728d34b10e0c918
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 16:56:45 +0200

    DUUDE

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        my_list.sort()
+        return (my_list[0])
+
+
+if __name__ == "__main__":
+    max_integer([1,35,3,5,2])
