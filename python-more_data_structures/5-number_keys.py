6e3b3d7827d7686aeefa4c0d5f90ea629e5da754
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-06-27 23:54:51 +0200

    Go

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def number_keys(a_dictionary):
+    count = 0
+    for key in a_dictionary:
+        count = count + 1
+    return count
+
+
+if __name__ == "__main__":
+    print(0)
