8aa75a26488ff8b3e3752504704f133dc1a69b91
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-07-04 12:50:11 +0200

    Type or Value

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError and TypeError:
+    except ValueError or TypeError:
         return False
 
 
