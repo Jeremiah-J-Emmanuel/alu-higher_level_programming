2de9acaf3c6ee9c7016ee2de06e1d5512d2adc35
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-07-04 13:13:24 +0200

    Fixed

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,9 +3,9 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError or TypeError:
+    except (ValueError, TypeError):
         return False
 
 
 if __name__ == "__main__":
-    print(safe_print_integer([1,2,4,4]))
+    print(safe_print_integer({89}))
