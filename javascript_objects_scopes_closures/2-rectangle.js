8aba74a162433aec298ac7983892052ed79a45f6
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-10-07 01:46:29 +0200

    Square

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -6,6 +6,16 @@ class Rectangle {
       this.height = h;
     }
   }
+
+  print() {
+    let i = 1
+    while (i < this.height) {
+        const str = "X";
+        const repeatedStr = str.repeat(this.width);
+        console.log(repeatedStr);
+        i++;
+    }
+  }
 }
 
 module.exports = Rectangle;
