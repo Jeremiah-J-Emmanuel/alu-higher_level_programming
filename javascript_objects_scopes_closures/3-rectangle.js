cb19dae9cd7cc5d54243e085484c4d22cf9ebc24
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-10-07 01:48:07 +0200

    Undefined

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    let i = 1
+    while (i < this.height) {
+        const str = "X";
+        const repeatedStr = str.repeat(this.width);
+        console.log(repeatedStr);
+        i++;
+    }
+  }
+}
+
+module.exports = Rectangle;
