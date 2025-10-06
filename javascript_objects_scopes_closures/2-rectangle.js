67ac2924f0744e090f99262dcccd70bc3a656452
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-10-07 01:20:32 +0200

    Second Rectangle

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (width <= 0 || !width <= 0) {
+      this.width = {};
+      this.height = {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+
+
+  }
+}
+
+module.exports = Rectangle;
